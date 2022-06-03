6b5c92a4c7ce4aa5983d2421d6f01b66a58db112
jlmansilla <juanmancillaster@gmail.com>
2022-06-03 11:12:55 -0400

    ejercicio video js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-var titulo=document.querySelector('h1');
-titulo.innerHTML='Hola Mundo!'
+
 
 //variables 
 function varTest(){
     var x = 10;
     if (true){
         var x = 15;
-        document.write(x "\n");        
+        document.write (x "\n");        
     }
-    document.write(x "\n");   
+    document.write (x "\n");   
 }
+
+function letTest(){
+    let y = 20;
+    if (true){
+        var y = 25;
+        document.write(y "\n");        
+    }
+    document.write(y "\n");   
+}
+varTest()
+letTest()
