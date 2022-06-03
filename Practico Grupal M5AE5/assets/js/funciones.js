916f824906ae44502bad16cb1bbff0fe52749949
Juan Luis Mansilla <juanmancillaster@gmail.com>
2022-06-03 09:45:38 +0000

    cambios funcional

--- Practico Grupal M5AE5/assets/js/funciones.js
+++ Practico Grupal M5AE5/assets/js/funciones.js
@@ -1,29 +1,29 @@
 function validar(){
 const btnEnviar = document.getElementById('btn-enviar');
 
-
+  const nombreFormulario = document.getElementById('formulario')
   const nombreDeUsuario = document.getElementById('usuario');
   const direcciónEmail = document.getElementById('email');
   const numeroTelefono = document.getElementById('telefono');
   if (usuario.value === "") {
     alert("Por favor, escribe tu nombre de usuario.");
-    usuario.focus();
+    nombreDeUsuario.focus();
     return false;
   }
 
   if (email.value === "") {
     alert("Por favor, escribe tu correo electrónico");
-    email.focus();
+    direcciónEmail.focus();
     return false;
   }
   
   if (telefono.value === "") {
     alert("Por favor, escribe tu número de teléfono");
-    telefono.focus();
+    numeroTelefono.focus();
     return false;
   }
   return true;
 
 
-//submitBtn.addEventListener('click', validate);
+
 }
