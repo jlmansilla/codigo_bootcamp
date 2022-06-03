bb1b078f42bbde4f3f94fbe9a832a12fe0ce9456
Juan Luis Mansilla <juanmancillaster@gmail.com>
2022-06-03 02:06:46 +0000

    prueba función

--- Practico Grupal M5AE5/assets/js/funciones.js
+++ Practico Grupal M5AE5/assets/js/funciones.js
@@ -1,21 +1,29 @@
+function validar(){
 const btnEnviar = document.getElementById('btn-enviar');
 
-const validación = (e) => {
-  e.preventDefault();
+
   const nombreDeUsuario = document.getElementById('usuario');
   const direcciónEmail = document.getElementById('email');
+  const numeroTelefono = document.getElementById('telefono');
   if (usuario.value === "") {
     alert("Por favor, escribe tu nombre de usuario.");
     usuario.focus();
     return false;
   }
+
   if (email.value === "") {
     alert("Por favor, escribe tu correo electrónico");
     email.focus();
     return false;
   }
   
+  if (telefono.value === "") {
+    alert("Por favor, escribe tu número de teléfono");
+    telefono.focus();
+    return false;
+  }
   return true;
-}
 
-submitBtn.addEventListener('click', validate);
+
+//submitBtn.addEventListener('click', validate);
+}
