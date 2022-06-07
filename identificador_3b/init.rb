cc99b27ffd6ec33d650de0b177dbdc070851c73d
Juan Luis Mansilla <juanmancillaster@gmail.com>
2022-06-07 23:52:04 +0000

    corrige ejercicio

--- identificador_3b/init.rb
+++ identificador_3b/init.rb
@@ -13,29 +13,41 @@ puts "-" * 10
 
 puts "Ingresa la clave:"
 clave = gets.chomp
+
 puts "Ingresa Nombre:"
 nombre = gets.chomp
 
 puts "Ingresa la marca:"
-marca = gets.chomp.
-puts "Ingresa Unidad:"
-unidad = gets.chomp
+marca = gets.chomp
 
 puts "Ingresa el precio:"
 precio = gets.chomp.to_i
+
+if precio < 1000
+    puts "¿La cantidad es en dólares? Si(1), No(2)"
+    respuesta= gets.chomp
+    if respuesta == "1"
+        precio_string = "#{precio} USD"
+    elsif respuesta == "2"
+        precio_string = "#{precio} CLP"
+    end
+else
+    precio_string = "#{precio} CLP"
+end
+
+puts "Ingresa Unidad:"
+unidad = gets.chomp
+
+
 puts "Ingresa categoria:"
 categoria = gets.chomp
 
-if precio < 1000
-    precio_tipo = " USD"
-else
-    precio_tipo += " CLP"
-end
 
-precio_string=precio.to_s
+
+#precio_string=precio.to_s
 puts "Clave: " + clave
 puts "Nombre: " + nombre
 puts "Marca: " + marca
-puts "Precio: " + precio_string + precio_tipo
+puts "Precio: " + precio_string
 puts "Unidad: " + unidad
 puts "Categoria: " + categoria
