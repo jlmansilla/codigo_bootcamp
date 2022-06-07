460fb7360c274af213de97e8721fe103070f328c
jlmansilla <juanmancillaster@gmail.com>
2022-06-07 17:55:31 -0400

    solucion parcial ejercicio ruby 2

--- ejercicio6terminacion.rb
+++ ejercicio6terminacion.rb
@@ -1,2 +1,27 @@
 a = "RRGGBBYYKK"
-20.times {puts #{a}}
+
+for i in 1..20
+    puts a
+
+
+letra = a [0]
+a = a[1...]
+a <<- letra
+
+
+
+end
+
+=begin
+    
+rescue => exception
+    
+end
+for i in 1..10
+    puts a
+    a[10-(i+1)] = a[a.length]
+   
+    
+    
+end
+=end
