4476eb94e15aac875d6ce2958dd36b4932e97758
Juan Luis Mansilla <juanmancillaster@gmail.com>
2022-06-08 01:28:00 +0000

    Co-authored-by: Marcos Antonio Sobarzo <MarcosSobarzo@users.noreply.github.com> Co-authored-by: Mary2agonz <Mary2agonz@users.noreply.github.com>cambios

--- identificador_3b/init.rb
+++ identificador_3b/init.rb
@@ -12,7 +12,7 @@ print "-" * 10
 print "Ingresa la siguiente información del producto"
 puts "-" * 10
 
-puts "\nIngresa la clave:"
+puts "Ingresa la clave:"
 clave = gets.chomp
 
 puts "Ingresa Nombre:"
@@ -25,37 +25,25 @@ puts "Ingresa Unidad:"
 unidad = gets.chomp
 
 puts "Ingresa el precio:"
-precio = gets.chomp.to_i
+precio = gets.chomp
 
-
-
-if precio < 1000
-    puts "¿La cantidad es en dólares? Si(1), No(2)"
+puts "¿La cantidad es en dólares? Si(1), No(2)"
     respuesta= gets.chomp
-    if respuesta == "1"
-        precio_string = "$#{precio} USD, Precios USD"
-        
-    elsif respuesta == "2"
-        precio_string = "$#{precio} CLP, Precios CLP"
-        
-    end
+if  respuesta == "1"
+    precio.to_i < 1000 
+    precio_string = precio + " USD"
 else
-    precio_string = "#{precio} CLP, Precios CLP"
-    
-    
+    precio_string = precio + " CLP"
 end
-
-
-
 
 puts "Ingresa categoria:"
 categoria = gets.chomp
 
 
-print "\n\n La información del producto capturada es:\n\n
+print "\n\nLa información del producto capturada es:
 clave: #{clave}
 nombre: #{nombre}
 marca: #{marca}
 unidad: #{unidad}
 precio: #{precio_string}
-categoria: #{categoria} \n\n"
+categoria: #{categoria}"
