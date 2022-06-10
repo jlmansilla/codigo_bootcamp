866eb9e30ec5170483ae9fab7bc492edae005839
Juan Luis Mansilla <juanmancillaster@gmail.com>
2022-06-10 23:42:26 +0000

    ejercicio grupal 3
    Co-authored-by: Marilyn Aguilar González~ <Mary2agonz@users.noreply.github.com>
    Co-authored-by: Marcos Antonio Sobarzo <MarcosSobarzo@users.noreply.github.com>cambios

---
+++ identificador_3b/init3.rb
@@ -0,0 +1,42 @@
+puts "-" * 60
+print "|"
+print " " * 8
+print "Bienvenido al identificador de precios 3b's"
+print " " * 7
+print "|"
+puts ""
+puts "-" *60
+puts
+puts
+print "-" * 10
+print "Catálogo de productos"
+puts "-" * 10
+
+const empresa = "3BS"
+
+
+
+def ingreso
+    
+end
+
+
+    
+
+
+    print "\n\nLos datos el producto son:
+    clave: #{clave}
+    nombre: #{nombre}
+    marca: #{marca}
+    unidad: #{unidad}
+    precio: #{precio_string}
+    categoria: #{categoria}\n"
+
+end
+
+print "-" * 16
+print "Las sumas totales son:"
+puts "-" * 8
+puts "La suma de los precios capturados en USD es: #{suma_usd}"
+puts "La suma de los precios capturados en CLP es: #{suma_clp}"
+
