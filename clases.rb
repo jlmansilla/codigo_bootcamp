ef035bdbd2c80c9166b389ebc0d3b0f9f501c506
jlmansilla <juanmancillaster@gmail.com>
2022-06-22 23:33:08 -0400

    ejercicio individual 7 ruby

--- clases.rb
+++ clases.rb
@@ -1,7 +1,7 @@
 class Radio
     attr_accessor :volumen, :frecuencia, :title 
 
-    def initialize(volumen, frecuencia) #constructor
+    def initialize() #constructor
         @volumen= volumen || 1 # Si no se ingresa un valor, se asigna 1
         @frecuencia= frecuencia || 'AM' # Si no se ingresa un valor, se asigna AM
         @title= title || "Radio Zero"
@@ -9,11 +9,31 @@ class Radio
     end
 
     def SubirVolumen(volumen) # Método para subir el volumen
-        
+        if @volumen + volumen > 30 
+            
+            puts "volumen  no valido"
+        else
+            @volumen += volumen
+        end
     end
 
     def BajarVolumen(volumen) # Método para bajar el volumen
+        if @volumen - volumen < 1
+            
+            puts "volumen  no valido"
+        else
+            @volumen -= volumen
+        end
+    end
 
+    def CambiarFrecuencia(freq) # Método para cambiar la frecuencia
+        if freq == 1
+            @frecuencia = "AM"
+        elsif freq == 2
+           @frecuencia = "FM"
+        else
+            puts "Frecuencia no valida"
+        end
     end
 
     def estatus # Método para mostrar el estatus de la radio
